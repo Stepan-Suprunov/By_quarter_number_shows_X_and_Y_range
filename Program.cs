37e68c7019e9fc3bbaeb7b914c9ee8b31d421d75
Stepan_Suprunov <ipsuprunovso@mail.ru>
2022-06-16 16:55:08 +0300

    Init commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿// По номеру четверти показывает диапазон X и Y.
+
+Console.WriteLine("Введите номер четверти:");
+int Quater = int.Parse(Console.ReadLine());
+
+if (Quater == 1)
+{
+    Console.WriteLine("Х от 0 до бесконечности");
+    Console.WriteLine("У от 0 до бесконечности");
+}
+else if (Quater == 2)
+{
+    Console.WriteLine("Х от 0 до минус бесконечности");
+    Console.WriteLine("У от 0 до бесконечности");
+}
+else if (Quater == 3)
+{
+    Console.WriteLine("Х от 0 до минус бесконечности");
+    Console.WriteLine("У от 0 до минус бесконечности");
+}
+else if (Quater == 4)
+{
+    Console.WriteLine("Х от 0 до бесконечности");
+    Console.WriteLine("У от 0 до минус бесконечности");
+}
+else
+{
+    Console.WriteLine("Введён не верный номер четвери");
+}
